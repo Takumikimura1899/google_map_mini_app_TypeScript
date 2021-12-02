058487423a58af9f2e6ea103e7bb690d5915f50b
Takumikimura141899 <kimura141899@gmail.com>
2021-12-03 04:58:47 +0900

    [add] ユーザー入力情報取得

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-// ここにコードを書きます
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address') as HTMLInputElement;
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enterdAddress = addressInput.value;
+
+  //   Google APIに送信
+}
+
+form.addEventListener('submit', searchAddressHandler);
